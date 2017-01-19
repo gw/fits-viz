fd502f31e8694ddc8321e15efd76180b4802835e
Grant Wu <grantwu2@gmail.com>
2017-01-19 11:51:44 -0800

    Minor refactor
    - Remove some semicolons

--- src/app.js
+++ src/app.js
@@ -4,25 +4,25 @@ import '../lib/fits'  // Creates window.astro
 import init from './babylonInit'  // Creates window.astro
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  let filePath = 'aored_When_2016.2.29_22.9.29_poldm_phase.fits';
-  let nFrames = 50; // Number of .fits data frames to load
-  let frames = [];  // Container for loaded .fits data frames
-  let nLoaded = 0;  // Number of .fits data frames loaded so far
-  let extent;       // [min, max] of values in a frame
+  let filePath = 'aored_When_2016.2.29_22.9.29_poldm_phase.fits'
+  let nFrames = 50 // Number of .fits data frames to load
+  let frames = []  // Container for loaded .fits data frames
+  let nLoaded = 0  // Number of .fits data frames loaded so far
+  let extent       // [min, max] of values in a frame
 
   // Initialize a new FITS File object
   let fits = new astro.FITS(filePath, function() {
-    let dataUnit = this.getDataUnit();
+    let dataUnit = this.getDataUnit()
     dataUnit.getFrames(0, nFrames, function (frame, opts) {
-      nLoaded++;
-      console.log('got frame');
+      nLoaded++
+      console.log('got frame')
       frames.push({
         extent: dataUnit.getExtent(frame),
         frame: frame
-      });
+      })
       if (nLoaded == nFrames) {
-        init(frames, nFrames);
+        init(frames, nFrames)
       }
-    });
-  });
+    })
+  })
 })
