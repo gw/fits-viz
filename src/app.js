8754de5f0071b92daff6b040e083aa1bf3493ec5
Grant Wu <grantwu2@gmail.com>
2017-01-19 22:34:14 -0800

    Minor refactor

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ import '../lib/fits'  // Creates window.astro
 import fitsInit from './fitsInit'
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById('fitsFile').addEventListener('change', function (e) {
-    let file = e.currentTarget.files[0]
-    fitsInit(file)
-  })
+  document.getElementById('fitsFile').addEventListener('change', function () {
+    fitsInit(this.files[0])
+  }, false)
 })
